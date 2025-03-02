ccf6a2020bc7d751799dc91289f7f1dc06914ce2
RPDJF <ruipaulo.unify@outlook.fr>
2025-03-02 20:13:13 +0100

    fix clock, cpu temp, ram usage and python script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+echo "installing pip3"
+sudo pacman -S python-pip
+
+echo "install module requests (as system package)"
+pip3 install requests ---break-system-packages
+
+echo "uploading conf to ~/.config/waybar"
 mkdir -p ~/.config/waybar
 cp -r config-gitlab.jsonc style.css config.jsonc scripts/ ~/.config/waybar
 
