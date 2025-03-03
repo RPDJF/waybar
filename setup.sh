d4db3102abd1358288c2d7e4e447c12ba203a651
RPDJF <ruipaulo.unify@outlook.fr>
2025-03-03 07:42:33 +0100

    added sensors

--- setup.sh
+++ setup.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
+echo "grant sudo"
+sudo echo "sudo granted"
+
 echo "installing pip3"
 sudo pacman -S python-pip --no-interaction
 
 echo "install module requests (as system package)"
 pip3 install requests --break-system-packages
+
+echo "installing sensors"
+sudo pacman -S lm_sensors --no-interaction
 
 echo "uploading conf to ~/.config/waybar"
 mkdir -p ~/.config/waybar
