fb05b93998531dd1c5369cf31a2583ab91df8d75
RPDJF <ruipaulo.unify@outlook.fr>
2025-03-03 15:15:17 +0100

    fix

--- setup.sh
+++ setup.sh
@@ -3,14 +3,17 @@
 echo "grant sudo"
 sudo echo "sudo granted"
 
+echo "installing necessary fonts"
+sudo pacman -Syu ttf-font-awesome
+
 echo "installing pip3"
-sudo pacman -S python-pip --no-interaction
+sudo pacman -S python-pip --noconfirm
 
 echo "install module requests (as system package)"
 pip3 install requests --break-system-packages
 
 echo "installing sensors"
-sudo pacman -S lm_sensors --no-interaction
+sudo pacman -S lm_sensors --noconfirm
 
 echo "uploading conf to ~/.config/waybar"
 mkdir -p ~/.config/waybar
